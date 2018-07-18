915821cd1624accc62eb7a66da41ac472223e927
Puneet Kashyap <pkashyaps@gmail.com>
2018-07-18 09:57:41 -0400

    added unit test for about us component

--- src/app/aboutus/aboutus.component.spec.ts
+++ src/app/aboutus/aboutus.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { AboutusComponent } from './aboutus.component';
+
 
 describe('AboutusComponent', () => {
   let component: AboutusComponent;
@@ -8,9 +9,9 @@ describe('AboutusComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AboutusComponent ]
+      declarations: [AboutusComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -23,8 +24,37 @@ describe('AboutusComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should display header About Us', () => {
+  it('should display title', () => {
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('About Us');
-  })
+    expect(compiled.querySelector('div.heading>h1').textContent).toEqual('About Us');
+  });
+
+  it('should display image for about us', () => {
+    const comp = new AboutusComponent();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('div.image>img').src).toEqual(comp.img);
+  });
+
+  it('should display header description in paragraph', () => {
+    const comp = new AboutusComponent();
+    let result = fixture.debugElement.queryAll(By.css('.content'));
+    expect(result[0].nativeElement.textContent).toContain(comp.header);
+  });
+
+  it('should display list of memberships', () => {
+    const comp = new AboutusComponent();
+    let result = fixture.debugElement.queryAll(By.css('.memberships'));
+    result.forEach((x, i) => {
+      expect(x.nativeElement.textContent).toContain(comp.members[i]);
+    })
+  });
+
+  it('should display descriptions of about us in paragraph', () => {
+    const comp = new AboutusComponent();
+    let result = fixture.debugElement.queryAll(By.css('.description'));
+    result.forEach((x, i) => {
+      expect(x.nativeElement.textContent).toContain(comp.description[i]);
+    })
+  });
+
 });
