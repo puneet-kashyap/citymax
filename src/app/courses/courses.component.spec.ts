70ccccfdd3b91da7ab0ba71d83899211e70d21e5
Puneet Kashyap <pkashyaps@gmail.com>
2018-07-20 09:46:49 -0400

    unit test added for courses component

--- src/app/courses/courses.component.spec.ts
+++ src/app/courses/courses.component.spec.ts
@@ -1,8 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
-import { MatCardHeader, MatCardContent, MatCardActions, MatCard } from '@angular/material';
-
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader } from '@angular/material';
+import { By } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { CoursesComponent } from './courses.component';
 
 
@@ -19,7 +19,7 @@ describe('CoursesComponent', () => {
         MatCardActions,
         MatCard
       ],
-      providers: [{provide: APP_BASE_HREF, useValue : '/' }],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
       imports: [
         RouterModule.forRoot(
           []
@@ -37,5 +37,37 @@ describe('CoursesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('courses names should be displayed', () => {
+    let result = fixture.debugElement.queryAll(By.css('mat-card-header.card>h3'));
+    result.forEach((x, i) => {
+      expect(x.nativeElement.textContent).toEqual(component.courses[i].name);
+    });
+  });
+
+  it('images for course should be displayed', () => {
+    let result = fixture.debugElement.queryAll(By.css('img'));
+    result.forEach((x, i) => {
+      expect(x.nativeElement.src).toEqual(component.courses[i].img);
+      expect(x.nativeElement.alt).toEqual(component.courses[i].name);
+    });
+  });
+
+  it('descriptions for course should be displayed', () => {
+    let result = fixture.debugElement.queryAll(By.css('mat-card-content>p'));
+    result.forEach((desc, i) => {
+      expect(desc.nativeElement.textContent).toEqual(component.courses[i].description);
+    });
+  });
+
+  it('deatils button for course should be displayed', () => {
+    let result = fixture.debugElement.queryAll(By.css('mat-card-actions>button'));
+    result.forEach((x, i) => {
+      // x.nativeElement.click();
+      fixture.whenStable().then(()=> {
+      });
+      expect(x.nativeElement.getAttribute('ng-reflect-router-link')).toEqual('/' + component.courses[i].id);
+    });
   });
 });
