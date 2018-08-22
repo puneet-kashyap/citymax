6a231a8c513ac38584332e143a97e017d722dd0c
Puneet Kashyap <pkashyaps@gmail.com>
2018-08-22 10:06:44 -0400

    chat bot updated to fetch message from backend

--- src/app/chat/dialogflow.service.ts
+++ src/app/chat/dialogflow.service.ts
@@ -1,49 +1,22 @@
 import { Injectable } from "@angular/core";
-import { Http, Headers } from "@angular/http";
-import { HttpModule } from "@angular/http";
-import { HttpClientModule } from "@angular/common/http";
+import { Http } from "@angular/http";
 import { map } from "rxjs/operators";
-import { Observable } from "rxjs";
-import { environment } from "../../environments/environment";
-import { dialogflow } from "dialogflow";
-declare var require: any;
 
 @Injectable({
   providedIn: "root"
 })
 export class DialogflowService {
-  private baseURL: string = "https://api.dialogflow.com/v1/query?v=20150910";
-  private token: string;
+  constructor(private http: Http) {}
 
-  constructor(private http: Http) {
-    this.getToken();
-  }
-
-  public getToken(): any {
-    return this.http.get("http://localhost:3000/config").subscribe(res => {
-      this.token = res["_body"];
-      console.log(this.token);
-    });
-  }
-
-  public getResponse(query: string) {
-    let data = {
-      query: query,
-      lang: "en",
-      sessionId: "12345"
-    };
+  public getResponse(msg): any {
     return this.http
-      .post(`${this.baseURL}`, data, { headers: this.getHeaders() })
+      .get(
+        `https://us-central1-citymax-faacf.cloudfunctions.net/api/chat?msg=${msg}`
+      )
       .pipe(
         map(res => {
-          return res.json();
+          return res["_body"];
         })
       );
-  }
-
-  public getHeaders() {
-    let headers = new Headers();
-    headers.append("Authorization", `Bearer ${this.token}`);
-    return headers;
   }
 }
