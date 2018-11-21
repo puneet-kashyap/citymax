af5d968b52e07e400a97f91ca456abae6012a90c
Puneet Kashyap <pkashyaps@gmail.com>
2018-11-21 09:15:48 -0500

    Fixed header padding for course header

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var require : any;
 
 @Component({
   selector: 'app-aboutus',
@@ -16,7 +17,7 @@ export class AboutusComponent implements OnInit {
   title = 'About Us';
   header = 'We were established in the year 2018 and is pioneer and leaders in Punjab state, India for referring students all across the globe. We hold a large number of representations for various universities, institute and schools all around the world. We are the only establishment in India, who are a member of all Chambers of Commerce & relevant associations & bodies in India and abroad. We hold a track record of sending huge number of students in the past.';
   img = {
-    'src': 'http://www.sehdeveducation.com/images/about_pic.png',
+    'src': require('../../assets/images/courses/undergraduate-course.jpg'),
     'alt': 'About CitiMax Enterprises'
   }
   description = [
